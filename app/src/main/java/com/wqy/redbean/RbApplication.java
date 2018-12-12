2021a156200015f95b37df963c151c0c29ea7a89
wuqiuyun <wuqiuyun_9629@163.com>
2018-12-12 16:31:42 +0800

    first commit

---
+++ app/src/main/java/com/wqy/redbean/RbApplication.java
@@ -0,0 +1,11 @@
+package com.wqy.redbean;
+
+import android.app.Application;
+
+/**
+ * Created by wqy on 2018/12/12.
+ */
+
+public class RbApplication extends Application {
+
+}
---
+++ app/src/main/java/com/wqy/redbean/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.wqy.redbean.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wqy on 2018/12/12.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
