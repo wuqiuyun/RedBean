4d9b0a56387ad0b138a23bc119a563fb5cf1fb43
wuqiuyun <wuqiuyun_9629@163.com>
2018-12-12 16:44:33 +0800

    mm

--- app/src/main/java/com/wqy/redbean/MainActivity.java
+++ app/src/main/java/com/wqy/redbean/MainActivity.java
@@ -1,13 +1,15 @@
 package com.wqy.redbean;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.wqy.redbean.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
